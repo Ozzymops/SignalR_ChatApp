3caa3000a54dec04a34b5b73eaf753339014aad3
Justin Muris <1736302muris@zuyd.nl>
2019-02-14 16:09:07 +0100

    Managed to get a PartialView inside a View

---
+++ SignalR_ChatApp/ViewComponents/UserComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_ChatApp.ViewComponents
+{
+    public class UserComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            Models.Database db = new Models.Database();
+            List<Models.User> userList = db.RetrieveUsers();
+            return View(userList);
+        }
+    }
+}
