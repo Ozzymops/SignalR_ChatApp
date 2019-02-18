198c22e3adcae6a2f6dbd80a1701b05f46c49711
Justin Muris <1736302muris@zuyd.nl>
2019-02-18 14:02:52 +0100

    Final edit

---
+++ SignalR_ChatApp/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_ChatApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            ViewBag.Title = "Login Page";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Pages.LoginModel lm)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _SignInManager.PasswordSignInAsync();
+            }
+            return View(lm);
+        }
+    }
+}
--- SignalR_ChatApp/Pages/Login.cshtml.cs
+++ SignalR_ChatApp/Pages/Login.cshtml.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SignalR_ChatApp.Pages
 {
     public class LoginModel : PageModel
     {
+        [Required]
         public string Username { get; set; }
 
-        [DataType(DataType.Password)]
+        [Required, DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Display(Name = "Remember me")]
